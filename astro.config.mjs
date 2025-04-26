991fa2f2082bb498fd32cd1620c36fc6dbb19f6a
erixk <erixk2807@gmail.com>
2025-04-27 00:00:05 +0100

    basic markup and some astro practice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          postcssPresetEnv({
+            stage: 0,
+            features: {
+              "custom-media-queries": true,
+            },
+          }),
+        ],
+      },
+    },
+  },
+});
