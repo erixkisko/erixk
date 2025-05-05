cc128a1eee20076e234a1244bc05536762353afd
erixk <erixk2807@gmail.com>
2025-05-05 21:07:40 +0100

    clean-up, mobile css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import postcssPresetEnv from "postcss-preset-env";
-import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,24 +17,6 @@ export default defineConfig({
           }),
         ],
       },
-    },
-  },
-  security: {
-    checkOrigin: true,
-  },
-  image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        pathname: "/**",
-      },
-    ],
-  },
-  markdown: {
-    remarkPlugins: [remarkGfm],
-    shikiConfig: {
-      theme: "github-dark",
     },
   },
 });
